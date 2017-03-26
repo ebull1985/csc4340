cb29346012daf9b2caa0d4db7955296114881699
ebull1985 <ebull@knights.ucf.edu>
2017-03-26 12:41:45 -0400

    Setting up hashtables

---
+++ src/SemanticException.java
@@ -0,0 +1,19 @@
+
+public class SemanticException extends Exception {
+	
+	public SemanticException() {
+		super();
+	}
+	
+	public SemanticException(String message) {
+		super(message);
+	}
+	
+	public SemanticException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public SemanticException(Throwable cause) {
+		super(cause);
+	}
+}
